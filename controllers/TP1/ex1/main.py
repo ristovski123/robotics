91c04f4713d43874a2c318dc492a949e1f23cf32
Miguelristo12 <midisgames@gmail.com>
2024-03-05 16:39:01 +0000

    commit

--- controllers/TP1/ex1/main.py
+++ controllers/TP1/ex1/main.py
@@ -6,7 +6,12 @@ By: Gonçalo Leão
 from controller import Robot
 from controllers.utils import cmd_vel
 
+
+
 # Create the Robot instance.
 robot: Robot = Robot()
 
+while True:
+    cmd_vel(robot, linear_vel=0.1, angular_vel=0)
+    robot.step()
 # TODO: Use cmd_vel and robot.step()
--- controllers/TP1/ex3/main.py
+++ controllers/TP1/ex3/main.py
@@ -4,9 +4,18 @@ By: Gonçalo Leão
 """
 
 import math
+import time
 from controller import Robot
 from controllers.utils import cmd_vel
 
+def simple_timer(seconds):
+    time.sleep(seconds)
+
 robot: Robot = Robot()
+timer = 0
+while True:
+    cmd_vel(robot, linear_vel=6, angular_vel=0)
+    robot.step()
+
 
 # TODO
